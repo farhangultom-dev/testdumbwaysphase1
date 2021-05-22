ac1936624fac94c22082ec922b8d4759f9d3e2f3
Muhammad Farhan Gultom <63413976+farhangultom-dev@users.noreply.github.com>
2021-05-22 20:03:47 +0700

    Add files via upload

---
+++ soal2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <?php
+
+    function urutKata($parameter)
+    {
+        echo $parameter;
+    }
+
+    urutKata("in1i ib2u bu3di");
+    ?>
+</body>
+
+</html>
